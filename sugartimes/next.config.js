587166a949c4ca1a352e3abe73fee42273eabf57
Savadiafoundation <savadiafoundation@gmail.com>
2026-04-02 21:23:01 +0530

    json

--- sugartimes/next.config.js
+++ sugartimes/next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // ✅ VERY IMPORTANT
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "i.pravatar.cc" },
     ],
+    unoptimized: true, // ✅ REQUIRED for static export
   },
 };
 
